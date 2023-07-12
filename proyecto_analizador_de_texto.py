eda11d3fef45c84ace80500cdafd54682c931593
edu33342 <ddp33342@gmail.com>
2023-07-11 20:29:12 -0400

    actualizacion de proyecto_analizadorDeTexto.py

--- proyecto_analizador_de_texto.py
+++ proyecto_analizador_de_texto.py
@@ -2,24 +2,37 @@ texto = input("Escriba un texto, poema, oracion, letra de musica o literalmente 
 
 #Contar cuantas veces se repiten las letras "a", "b" y "c".
 
-texto.lower()
-contador_a = texto.count("a")
-contador_b = texto.count("b")
-contador_c = texto.count("c")
+letras = []
 
-contador = f"La letra 'a' se repite {contador_a} veces, la 'b' {contador_b} veces y la 'c' {contador_c} veces"
+texto.lower()
+
+print("Elije letras a eleccion para contarlas: ")
+print("")
+letras.append(input("Ingresa la primera letra: ").lower())
+letras.append(input("Ingresa la segunda letra: ").lower())
+letras.append(input("Ingresa la tercera letra: ").lower())
+print("")
+print("CANTIDAD DE LETRAS")
+contador1 = texto.count(letras[0])
+contador2 = texto.count(letras[1])
+contador3 = texto.count(letras[2])
+
+contador = f"La letra '{letras[0]}' se repite {contador1} veces, la '{letras[1]}' {contador2} veces y la '{letras[2]}' {contador3} veces"
 print(contador)
+print("")
 
 #Contar cuantas palabras hay en el texto.
 
 palabras = texto.split()
 print(f"En el texto hay {len(palabras)} palabras")
+print("")
 
 #Identificar primera y ultima letra
 
 primera_letra = texto[0]
 ultima_letra = texto[-1]
 print(f"La primera letra del texto es {primera_letra} y la ultima es {ultima_letra}")
+print("")
 
 #palabras en orden inverso
 
@@ -27,6 +40,7 @@ palabra = texto.split()
 palabra_invertida = reversed(palabra)
 texto_invertido = " ".join(palabra_invertida)
 print(f"Texto invertido: {texto_invertido}")
+print("")
 
 #Identificar si aparece la palabra "python"
 
